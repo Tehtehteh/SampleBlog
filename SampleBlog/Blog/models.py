99398686a25ce4eca267d7e4d76c8ef59e73fb05
Tehtehteh <tehfvpr@gmail.com>
2016-09-09 12:17:12 +0300

    Changed __str__ and added on delete cascade.

--- SampleBlog/Blog/models.py
+++ SampleBlog/Blog/models.py
@@ -4,12 +4,12 @@ class Subject(models.Model):
     subject = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.subject
+        return self.subject[0].upper() + self.subject[1:].lower()
 
 class BlogPost(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
     title = models.CharField(max_length=25)
-    subject = models.ForeignKey(Subject)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     text = models.TextField(null=False)
     pic = models.ImageField(upload_to='static/img/', default='static/img/no-image.png')
     date = models.DateTimeField()
